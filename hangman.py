7bdbc3474c4fcc99dba055af289851996bd76f9a
Carl Byrd <carlbyrd64@outlook.com>
2017-12-26 23:43:34 -0500

    hangman initial commit

---
+++ hangman.py
@@ -0,0 +1,21 @@
+print('Welcome to Hangman!')
+answer = 'this place'
+answer = {
+  "t" : False,
+  "h" : False,
+  "i" : False,
+  "s" : False,
+  " " : True,
+  "p" : False,
+  "l" : False,
+  "a" : False,
+  "c" : False,
+  "e" : False,
+  
+}
+
+
+while gameActive == True:
+  userInput = input('Try a letter')
+  
+  
